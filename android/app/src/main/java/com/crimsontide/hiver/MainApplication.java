ba9bc7825607a1a732a37dc2589075cd7b1fa5af
Ben Mechen <ben@chelsea-apps.com>
2022-08-16 15:48:49 +0100

    Upgrade reanimated

--- android/app/src/main/java/com/crimsontide/hiver/MainApplication.java
+++ android/app/src/main/java/com/crimsontide/hiver/MainApplication.java
@@ -14,7 +14,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -37,11 +36,6 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
-        }
-
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-            return new ReanimatedJSIModulePackage();
         }
       };
 
